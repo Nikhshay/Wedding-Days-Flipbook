f1c35895b05bef90177e55e831b93a7713770383
Nikhshay Singh Kukreja <nikhshaykukreja@gmail.com>
2025-04-28 19:57:10 +0700

    Good Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,24 @@ $(window).ready(function() {
 });
 
 
-$(window).bind('keydown', function(e){
-    
-    if (e.keyCode==37)
+$(window).on('keydown', function(e) {
+    // Check if the pressed key is the left or right arrow key
+    if (e.keyCode === 37) {
         $('#magazine').turn('previous');
-    else if (e.keyCode==39)
+        // Prevent the default action of the left arrow key (scrolling the page)
+        e.preventDefault();
+    } else if (e.keyCode === 39) {
         $('#magazine').turn('next');
-        
+        // Prevent the default action of the right arrow key (scrolling the page)
+        e.preventDefault();
+    }
+});
+
+const initialWidth = window.innerWidth;
+const initialHeight = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth !== initialWidth || window.innerHeight !== initialHeight) {
+    window.location.reload();
+  }
 });
