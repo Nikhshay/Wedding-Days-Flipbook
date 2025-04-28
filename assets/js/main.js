74e899dc22630647ed258f145038219becd353cb
Nikhshay Singh Kukreja <nikhshaykukreja@gmail.com>
2025-04-28 21:59:06 +0700

    Added Mobile scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,38 @@
 $(window).ready(function() {
-    $('#magazine').turn({
-                        display: 'double',
-                        acceleration: true,
-                        gradients: !$.isTouch,
-                        elevation:50,
-                        when: {
-                            turned: function(e, page) {
-                                /*console.log('Current view: ', $(this).turn('view'));*/
-                            }
-                        }
-                    });
-});
+    if (window.innerWidth > 768) { 
+        $('#magazine').turn({
+            display: 'double',
+            acceleration: true,
+            gradients: !$.isTouch,
+            elevation: 50,
+            when: {
+                turned: function(e, page) {}
+            }
+        });
 
+        $(window).on('keydown', function(e) {
+            if (e.keyCode === 37) {
+                $('#magazine').turn('previous');
+                e.preventDefault();
+            } else if (e.keyCode === 39) {
+                $('#magazine').turn('next');
+                e.preventDefault();
+            }
+        });
 
-$(window).on('keydown', function(e) {
-    // Check if the pressed key is the left or right arrow key
-    if (e.keyCode === 37) {
-        $('#magazine').turn('previous');
-        // Prevent the default action of the left arrow key (scrolling the page)
-        e.preventDefault();
-    } else if (e.keyCode === 39) {
-        $('#magazine').turn('next');
-        // Prevent the default action of the right arrow key (scrolling the page)
-        e.preventDefault();
+    } else {
+        // Mobile view
+        $('#magazine').addClass('simple-scroll');
+        $('html, body').css({
+            'overflow-y': 'auto', // Enable vertical scrolling
+            'overflow-x': 'hidden',
+            'height': 'auto'
+        });
     }
 });
 
 const initialWidth = window.innerWidth;
 const initialHeight = window.innerHeight;
-
 window.addEventListener('resize', () => {
   if (window.innerWidth !== initialWidth || window.innerHeight !== initialHeight) {
     window.location.reload();
